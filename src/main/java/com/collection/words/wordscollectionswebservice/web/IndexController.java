9a5698420c1fe19e2a7216c6f09ef5ebd429a0f5
sky72145 <sky7214sky72@naver.com>
2021-07-14 17:51:23 +0900

    20210714 최원석 nodejs 설치

--- src/main/java/com/collection/words/wordscollectionswebservice/web/IndexController.java
+++ src/main/java/com/collection/words/wordscollectionswebservice/web/IndexController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.net.http.HttpResponse;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
